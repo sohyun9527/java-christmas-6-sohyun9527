12e81659541a8a44c8766fcd1568c0125555e41e
현 <sohyun95277@gmail.com>
2023-11-10 20:36:16 +0900

    feat(InputView): 메뉴와 주문 개수를 입력받는 기능

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,13 +1,19 @@
 package christmas.view;
 
 import static christmas.view.message.ErrorMessage.INVALID_BLANK;
-import static christmas.view.message.ErrorMessage.NOT_ONLY_DIGIT;
+import static christmas.view.message.ErrorMessage.INVALID_NAME_COUNT_TYPE;
+import static christmas.view.message.ErrorMessage.INVALID_NUMBER_TYPE;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class InputView {
     private static final String DELIMITER = ",";
     private static final String DASH = "-";
+    private static final int VALID_SIZE = 2;
 
     public int readVisitDay() {
         String input = Console.readLine();
@@ -24,7 +30,48 @@ public class InputView {
 
     public static void validateOnlyDigit(String input) {
         if (!input.chars().allMatch(Character::isDigit) || input.isEmpty()) {
-            throw new NumberFormatException(NOT_ONLY_DIGIT.getMessage());
+            throw new NumberFormatException(INVALID_NUMBER_TYPE.getMessage());
         }
     }
+
+    public List<Map<String, Integer>> readOrders() {
+        String input = Console.readLine();
+        validateEmptyLine(input);
+        return getValidMenus(input);
+    }
+
+    public List<Map<String, Integer>> getValidMenus(String orders) {
+        return Arrays.stream(orders.split(DELIMITER))
+                .map(this::parseNameAndCount)
+                .map(this::createValidOrders)
+                .toList();
+    }
+
+    private List<String> parseNameAndCount(String name) {
+        List<String> nameAndCount = Arrays.asList(name.split(DASH));
+        validateNameAndCountLength(nameAndCount);
+        return nameAndCount;
+    }
+
+    private Map<String, Integer> createValidOrders(List<String> namedAndCount) {
+        String name = namedAndCount.get(0);
+        String count = namedAndCount.get(1);
+        int validatedCount = validateCount(count);
+
+        Map<String, Integer> menu = new HashMap<>();
+        menu.put(name, validatedCount);
+        return menu;
+    }
+
+    private void validateNameAndCountLength(List<String> order) {
+        if (order.size() != VALID_SIZE) {
+            throw new IllegalArgumentException(INVALID_NAME_COUNT_TYPE.getMessage());
+        }
+    }
+
+    private int validateCount(String count) {
+        validateOnlyDigit(count);
+        return Integer.parseInt(count);
+    }
+
 }
--- src/main/java/christmas/view/message/ErrorMessage.java
+++ src/main/java/christmas/view/message/ErrorMessage.java
@@ -3,7 +3,8 @@ package christmas.view.message;
 public enum ErrorMessage {
     PREFIX("[ERROR] "),
     INVALID_BLANK("아무것도 입력하지 않았습니다."),
-    NOT_ONLY_DIGIT("숫자만 입력해주세요."),
+    INVALID_NUMBER_TYPE("숫자의 형식이 올바르지 않습니다."),
+    INVALID_NAME_COUNT_TYPE("메뉴와 주문 개수는 (-)로 구분해주세요"),
     DUPLICATE_MENU("중복된 메뉴가 존재합니다.");
 
     private final String message;
