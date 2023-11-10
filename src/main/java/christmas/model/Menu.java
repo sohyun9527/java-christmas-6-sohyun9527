8982fb04342289b6d2fd1e55a29a6dad0d639f96
현 <sohyun95277@gmail.com>
2023-11-11 00:06:08 +0900

    feat(Orders): 중복메뉴 검증 기능

---
+++ src/main/java/christmas/model/Menu.java
@@ -0,0 +1,30 @@
+package christmas.model;
+
+public class Menu {
+    private static final int MINIMUM_COUNT = 1;
+    private final String name;
+    private final int count;
+
+    public Menu(String name, int count) {
+        validateContainMenu(name);
+        this.name = name;
+        validateOrderCount(count);
+        this.count = count;
+    }
+
+    private void validateOrderCount(int count) {
+        if (count < MINIMUM_COUNT) {
+            throw new IllegalArgumentException("최소 1개 이상 주문해야합니다.");
+        }
+    }
+
+    private void validateContainMenu(String name) {
+        if (MenuBoard.getByName(name) == null) {
+            throw new IllegalArgumentException(name + "은 존재하지 않는 메뉴입니다.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/main/java/christmas/model/Orders.java
+++ src/main/java/christmas/model/Orders.java
@@ -2,6 +2,7 @@ package christmas.model;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Orders {
@@ -9,6 +10,7 @@ public class Orders {
 
     public Orders(List<Map<String, Integer>> orders) {
         makeMenus(orders);
+        validateDuplicateMenu();
     }
 
     public void makeMenus(List<Map<String, Integer>> orders) {
@@ -16,5 +18,15 @@ public class Orders {
                 .flatMap(order -> order.entrySet().stream())
                 .map(entry -> new Menu(entry.getKey(), entry.getValue()))
                 .collect(Collectors.toList());
+    }
+
+    public void validateDuplicateMenu() {
+        Set<String> uniqueNames = menus.stream()
+                .map(Menu::getName)
+                .collect(Collectors.toSet());
+        
+        if (uniqueNames.size() != menus.size()) {
+            throw new IllegalArgumentException("중복된 메뉴가 존재합니다.");
+        }
     }
 }
