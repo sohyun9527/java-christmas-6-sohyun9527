692a62a67c9a650ff7f38a73e97f287d47032d3c
현 <sohyun95277@gmail.com>
2023-11-11 12:02:32 +0900

    feat(Discount): 날짜에 따른 할인 기능
    크리스마스 이전까지 추가 할인 적용,
    평일이라면 디저트 할인
    주말이라면 메인 할인
    별이 그려진 날이라면 특별할인

--- src/main/java/christmas/model/Discount.java
+++ src/main/java/christmas/model/Discount.java
@@ -1,18 +1,55 @@
 package christmas.model;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Discount {
-    private static final int YEAR = 2023;
+    private static final int FIRST_DAY = 1;
+    private static final int MINIMUM_SALE = 1000;
+    private static final int SALE_PRICE = 2023;
     private final Map<MenuBoard, Integer> bill;
-    private final int visitDate;
 
-    public Discount(Map<MenuBoard, Integer> bill, int date) {
+    public Discount(Map<MenuBoard, Integer> bill) {
         this.bill = bill;
-        this.visitDate = date;
+    }
+
+    public List<Long> discountResult(long price, Date date) {
+        List<Long> result = new ArrayList<>();
+        result.add(christmas(date));
+        result.add(commonDate(date));
+        result.add(weekendDate(date));
+        result.add(starDate(price, date));
+
+        return result;
+    }
+
+    public long christmas(Date date) {
+        if (date.isBeforeChristmas()) {
+            return MINIMUM_SALE + (date.getDate() - FIRST_DAY) * 100L;
+        }
+        return 0;
+    }
+
+    public long commonDate(Date date) {
+        if (date.isCommonDate()) {
+            return dessertCount() * SALE_PRICE;
+        }
+        return 0;
+    }
+
+    public long starDate(long beforeDiscountPrice, Date date) {
+        if (date.isStarDate()) {
+            return beforeDiscountPrice - MINIMUM_SALE;
+        }
+        return 0;
+    }
+
+    public long weekendDate(Date date) {
+        if (date.isWeekend()) {
+            return mainCount() * SALE_PRICE;
+        }
+        return 0;
     }
 
     public long beforeSalePrice() {
@@ -21,8 +58,16 @@ public class Discount {
                 .sum();
     }
 
-    public DayOfWeek getLocalDate() {
-        LocalDate localDate = LocalDate.of(YEAR, Month.DECEMBER, visitDate);
-        return localDate.getDayOfWeek();
+    public long dessertCount() {
+        return bill.entrySet().stream()
+                .filter(entry -> entry.getKey().getCategory() == Category.DESSERT)
+                .count();
     }
+
+    public long mainCount() {
+        return bill.entrySet().stream()
+                .filter(entry -> entry.getKey().getCategory() == Category.MAIN)
+                .count();
+    }
+
 }
