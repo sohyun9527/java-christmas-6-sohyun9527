704b395b55b934a46de30e559d988f1db86aecc8
현 <sohyun95277@gmail.com>
2023-11-11 18:02:22 +0900

    feat(ErrorMessage): 유효하지 않은 날짜 메세지 추가

--- src/main/java/christmas/view/message/ErrorMessage.java
+++ src/main/java/christmas/view/message/ErrorMessage.java
@@ -3,7 +3,7 @@ package christmas.view.message;
 public enum ErrorMessage {
     PREFIX("[ERROR] "),
     INVALID_BLANK("아무것도 입력하지 않았습니다."),
-    INVALID_DATE_RANGE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     INVALID_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     OVER_QUANTITY_ORDER("20개를 초과하여 주문할 수 없습니다."),
     CANT_ONLY_DRINKS_ORDER("음료만 주문할 수 없습니다.");
