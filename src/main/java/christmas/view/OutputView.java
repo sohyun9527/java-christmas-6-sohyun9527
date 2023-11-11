6a102b9e14dda079ce26fd7f97eac2bd78b478d7
현 <sohyun95277@gmail.com>
2023-11-11 17:55:22 +0900

    refactor(OutputView): enum 객체의 메세지 사용

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,7 +1,17 @@
 package christmas.view;
 
+import static christmas.view.message.Message.BADGE;
+import static christmas.view.message.Message.BEFORE_DISCOUNT;
+import static christmas.view.message.Message.COUNT;
+import static christmas.view.message.Message.DISCOUNT_PAYMENT;
+import static christmas.view.message.Message.DISCOUNT_PRICE;
+import static christmas.view.message.Message.DISCOUNT_RESULT;
+import static christmas.view.message.Message.NONE;
+import static christmas.view.message.Message.ORDER;
+import static christmas.view.message.Message.PROMOTION;
 import static christmas.view.message.Message.REQUEST_MENU_COUNT;
 import static christmas.view.message.Message.REQUEST_VISIT_DAY;
+import static christmas.view.message.Message.SHOW_EVENT_MESSAGE;
 import static christmas.view.message.Message.START_MESSAGE;
 
 import christmas.model.Menu;
@@ -10,54 +20,73 @@ import java.text.DecimalFormat;
 import java.util.List;
 
 public class OutputView {
+    private static final String PREFIX = "<";
+    private static final String POSTFIX = ">";
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printNewLine() {
+        System.out.println();
+    }
+
     public void printStartMessage() {
-        System.out.println(START_MESSAGE);
+        System.out.println(START_MESSAGE.getMessage());
     }
 
     public void printRequestVisitDay() {
-        System.out.println(REQUEST_VISIT_DAY);
+        System.out.println(REQUEST_VISIT_DAY.getMessage());
     }
 
     public void printRequestOrder() {
-        System.out.println(REQUEST_MENU_COUNT);
+        System.out.println(REQUEST_MENU_COUNT.getMessage());
     }
 
     public void printEventPreview(int day) {
-        String message = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+        String message = SHOW_EVENT_MESSAGE.getMessage();
         String result = String.format(message, day);
         System.out.println(result);
     }
 
     public void printOrders(List<Menu> orders) {
-        System.out.println("<주문 메뉴>");
+        System.out.println("\n" + PREFIX + ORDER.getMessage() + POSTFIX);
         for (Menu menu : orders) {
-            System.out.println(menu.getName() + " " + menu.getCount() + "개");
+            System.out.println(menu.getName() + " " + menu.getCount() + COUNT.getMessage());
         }
     }
 
     public void printBill(long price) {
-        System.out.println("<할인 전 총주문 금액>");
-        System.out.println(price + "원");
+        System.out.println(resultFormatter(BEFORE_DISCOUNT.getMessage()));
+        System.out.println(priceFormatter(price));
     }
 
     public void printPromotion(MenuBoard menu) {
-        System.out.println("<증정 메뉴>");
+        System.out.println(resultFormatter(PROMOTION.getMessage()));
         System.out.println(menu.getName());
     }
 
+    public String priceFormatter(long price) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,###원");
+        return decimalFormat.format(price);
+    }
+
+    public String resultFormatter(String message) {
+        return "\n" + PREFIX + message + POSTFIX;
+    }
+
     public void printDiscountResult(List<Long> discountPrice) {
-        System.out.println("<혜택 내역>");
+        System.out.println(resultFormatter(DISCOUNT_RESULT.getMessage()));
         if (discountPrice.isEmpty()) {
-            System.out.println("없음");
+            System.out.println(NONE.getMessage());
             return;
         }
-        DecimalFormat df = new DecimalFormat("#,###원");
-        List<String> discountTypes = List.of("크리스마스 할인: ", "평일 할인: ", "주말 할인: ", "특별 할인: ", "증정 이벤트: ");
+        List<String> discountTypes = List.of("크리스마스 할인:", "평일 할인:", "주말 할인:", "특별 할인:", "증정 이벤트:");
 
         for (int i = 0; i < discountPrice.size(); i++) {
             long discount = discountPrice.get(i);
             if (discount != 0) {
-                String formattedDiscount = df.format(discount);
+                String formattedDiscount = priceFormatter(discount);
                 String message = String.format("%s -%s", discountTypes.get(i), formattedDiscount);
                 System.out.println(message);
             }
@@ -65,21 +94,19 @@ public class OutputView {
     }
 
     public void printDiscountPrice(List<Long> result) {
-        System.out.println("<총혜택 금액>");
+        System.out.println(resultFormatter(DISCOUNT_PRICE.getMessage()));
         long price = result.stream().mapToLong(Long::longValue).sum();
-        System.out.println(price + "원");
+        System.out.println(priceFormatter(price));
 
     }
 
     public void printAfterDiscountPrice(long price) {
-        System.out.println("<할인 후 예상 결제 금액>");
-        DecimalFormat df = new DecimalFormat("#,###원");
-        String format = df.format(price);
-        System.out.println(format);
+        System.out.println(resultFormatter(DISCOUNT_PAYMENT.getMessage()));
+        System.out.println(priceFormatter(price));
     }
 
     public void printEventBadge(String badge) {
-        System.out.println("<12월 이벤트 배지>");
+        System.out.println(resultFormatter(BADGE.getMessage()));
         System.out.println(badge);
     }
 
