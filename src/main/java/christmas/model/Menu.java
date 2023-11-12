b44855693125b251f17ae274167c554bb97e70f5
현 <sohyun95277@gmail.com>
2023-11-12 11:32:17 +0900

    feat(Menu): 디저트, 메인 카테고리 판별 기능
    인스턴스 필드에 Category 추가,
    
    현재 주문이 메인 / 디저트 카테고리인지 판별하는 기능 추가

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -6,12 +6,14 @@ public class Menu {
     private static final int MINIMUM_COUNT = 1;
     private final String name;
     private final int count;
+    private final Category category;
 
     public Menu(String name, int count) {
         validateContainMenu(name);
         this.name = name;
         validateOrderCount(count);
         this.count = count;
+        this.category = MenuBoard.getCategoryByName(name);
     }
 
     private void validateOrderCount(int count) {
@@ -24,6 +26,14 @@ public class Menu {
         if (MenuBoard.getByName(name) == MenuBoard.NONE) {
             throw new IllegalArgumentException(INVALID_ORDER.getMessage());
         }
+    }
+
+    public boolean isDessert() {
+        return this.category.equals(Category.DESSERT);
+    }
+
+    public boolean isMain() {
+        return this.category.equals(Category.MAIN);
     }
 
     public String getName() {
