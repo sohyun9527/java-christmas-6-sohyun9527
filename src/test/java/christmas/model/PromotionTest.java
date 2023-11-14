51d9c48267bf3c4f0c29a1aabea527298b420f49
현 <sohyun95277@gmail.com>
2023-11-14 17:52:39 +0900

    test(PromotionTest): 샴페인 증정 기능 테스트 작성

--- src/test/java/christmas/model/PromotionTest.java
+++ src/test/java/christmas/model/PromotionTest.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class BadgeTest {
+class PromotionTest {
 
     @DisplayName("2만원 이상이라면 산타를 반환한다")
     @Test
     void getSanta() {
-        String badge = Badge.getBadge(20000);
+        String badge = Promotion.getBadgeByPrice(20000);
 
         assertEquals("산타", badge);
     }
@@ -18,7 +18,7 @@ class BadgeTest {
     @DisplayName("1만원 이상이라면 트리를 반환한다")
     @Test
     void getTree() {
-        String badge = Badge.getBadge(10000);
+        String badge = Promotion.getBadgeByPrice(10000);
 
         assertEquals("트리", badge);
     }
@@ -26,7 +26,7 @@ class BadgeTest {
     @DisplayName("5천원 이상이라면 별을 반환한다")
     @Test
     void getStar() {
-        String badge = Badge.getBadge(5000);
+        String badge = Promotion.getBadgeByPrice(5000);
 
         assertEquals("별", badge);
     }
@@ -34,8 +34,18 @@ class BadgeTest {
     @DisplayName("아무것도 해당하지 않는다면 없음을 반환한다")
     @Test
     void getNone() {
-        String badge = Badge.getBadge(4999);
+        String badge = Promotion.getBadgeByPrice(4999);
 
         assertEquals("없음", badge);
+    }
+
+    @DisplayName("총 금액이 120,000원을 넘는다면 샴페인 1개를 얻는다")
+    @Test
+    void getChampagne() {
+        long underPrice = 119_999;
+        long overPrice = 120_000;
+
+        assertEquals("없음", Promotion.promotionEvent(underPrice));
+        assertEquals("샴페인 1개", Promotion.promotionEvent(overPrice));
     }
 }
