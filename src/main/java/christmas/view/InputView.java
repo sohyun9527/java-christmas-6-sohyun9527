93ccdf7cccbb3deeb26e6e960f38062c498a0eb2
현 <sohyun95277@gmail.com>
2023-11-15 16:57:57 +0900

    refactor(InputView): 상수 메세지 Message enum 포함

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,17 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.model.exception.BlankException;
 
 public class InputView {
-    private static final String VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    private static final String REQUEST_ORDER_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     public String getVisitDate() {
-        String input = printAndReadInput(VISIT_DATE_MESSAGE);
+        String input = printAndReadInput(Message.VISIT_DATE_MESSAGE.getMessage());
         validateEmptyLine(input);
         return input;
     }
 
     public String getOrders() {
-        String input = printAndReadInput(REQUEST_ORDER_MESSAGE);
+        String input = printAndReadInput(Message.REQUEST_ORDER_MESSAGE.getMessage());
         validateEmptyLine(input);
         return input;
     }
--- src/main/java/christmas/view/Message.java
+++ src/main/java/christmas/view/Message.java
@@ -2,11 +2,13 @@ package christmas.view;
 
 public enum Message {
 
-    PREFIX("<"),
-    POSTFIX(">"),
+    VISIT_DATE_MESSAGE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
+    REQUEST_ORDER_MESSAGE("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+    
     START_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
     EVENT_PREVIEW_MESSAGE("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     ORDER("주문 메뉴"),
+    NONE("없음"),
     TOTAL_ORDER_AMOUNT("할인 전 총주문 금액"),
     PROMOTION("증정 메뉴"),
     DISCOUNT("혜택 내역"),
@@ -18,18 +20,6 @@ public enum Message {
 
     Message(String message) {
         this.message = message;
-    }
-
-    public static String priceFormatter(long price) {
-        return String.format("%,d원", price);
-    }
-
-    public static String menuFormatter(String name, int count) {
-        return String.format(name + " " + count + "개");
-    }
-
-    public static String titleNameFormatter(String message) {
-        return System.lineSeparator() + PREFIX.message + message + POSTFIX.message;
     }
 
     public String getMessage() {
