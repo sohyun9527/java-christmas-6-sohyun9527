ef227e32384af8ddf72b5a47ecd75b9bf18cadab
현 <sohyun95277@gmail.com>
2023-11-15 16:00:03 +0900

    feat(OrderInput): 하나의 메뉴에 대한 검증을 하는 기능
    쉼표로 자른 후 메뉴-개수 validation 진행

---
+++ src/main/java/christmas/model/OrderInput.java
@@ -0,0 +1,33 @@
+package christmas.model;
+
+import christmas.model.exception.OrderException;
+import java.util.List;
+
+public class OrderInput {
+    private static final String NAME_QUANTITY_DELIMITER = "-";
+    private static final int VALID_SIZE = 2;
+    private final String name;
+    private final int quantity;
+
+    public OrderInput(String input) {
+        List<String> nameAndQuantity = List.of(input.split(NAME_QUANTITY_DELIMITER, -1));
+        validateInputMenuSize(nameAndQuantity);
+        this.name = nameAndQuantity.get(0);
+        this.quantity = convertQuantity(nameAndQuantity.get(1));
+    }
+
+    public void validateInputMenuSize(List<String> nameAndQuantity) {
+        if (nameAndQuantity.size() != VALID_SIZE) {
+            throw new OrderException();
+        }
+    }
+
+    public int convertQuantity(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new OrderException();
+        }
+    }
+
+}
